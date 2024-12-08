7a2f0f9c55be01e12b41c88471d73c595d3b398b
Pritom Chowdhury <coderpritom03@gmail.com>
2024-12-08 07:28:13 -0800

    React Router Add it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import './index.css';
-
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import Movie from './pages/Movie'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 export default function App() {
-  return (
-    <div>
-      Movie Flix
-    </div>
-  );
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />
+    },
+    {
+      path: '/about',
+      element: <About />
+    },
+    {
+      path: '/movie',
+      element: <Movie />
+    },
+    {
+      path: '/contact',
+      element: <Contact />
+    }
+  ])
+
+  return <RouterProvider router={router}/>
 }
