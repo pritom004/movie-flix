bd5287b6f29a68e61ca9448e7aa3950c663d1dc6
Pritom Chowdhury <coderpritom03@gmail.com>
2024-12-11 07:40:34 -0800

    Header & Footer Ui add it

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -1,7 +1,72 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { CiFacebook} from "react-icons/ci";
+import { RxTwitterLogo } from "react-icons/rx";
+import { PiYoutubeLogo } from "react-icons/pi";
+import { IoLogoInstagram } from "react-icons/io5";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='bg-gray-900 py-10'>
+    <footer className='w-3/5 mx-auto px-32 '>
+      <div className='flex space-x-2 text-3xl text-white'>
+      <CiFacebook />
+      <IoLogoInstagram />
+      <RxTwitterLogo/>
+      <PiYoutubeLogo />
+      </div>
+
+    <div className='mt-5 flex w-full justify-between text-gray-300 font-semibold'>
+    <ul className='space-y-1'>
+      <li>
+        <NavLink>Audio Description</NavLink>
+      </li>
+      <li>
+        <NavLink>Inverstor</NavLink>
+      </li>
+      <li>
+        <NavLink>Legal Notices</NavLink>
+      </li>
+    </ul>
+
+    <ul className='space-y-1'>
+      <li>
+        <NavLink>Help Center</NavLink>
+      </li>
+      <li>
+        <NavLink>Jobs</NavLink>
+      </li>
+      <li>
+        <NavLink>Cookie Preferences</NavLink>
+      </li>
+    </ul>
+
+    <ul className='space-y-1'>
+      <li>
+        <NavLink>Gift Cards</NavLink>
+      </li>
+      <li>
+        <NavLink>Term of Use</NavLink>
+      </li>
+      <li>
+        <NavLink>Corporate Information</NavLink>
+      </li>
+    </ul>
+
+    <ul className='space-y-1'>
+      <li>
+        <NavLink>Media Centre</NavLink>
+      </li>
+      <li>
+        <NavLink>Privacy</NavLink>
+      </li>
+      <li>
+        <NavLink>Contact Us</NavLink>
+      </li>
+    </ul>
+
+    </div>
+    </footer>
+    </div>
   )
 }
--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
-
+import { NavLink } from 'react-router-dom'
 export default function Header() {
   return (
-    <div>Header</div>
+    <div>
+
+
+      <div className='flex w-[97%] mx-auto py-4 px-56 bg-gray-900 text-white justify-between text-base'>
+      <NavLink>Get Movie Flix Membership. 30-days return or refund guarantee</NavLink> 
+    <div className='text-indigo-500 space-x-4'>
+      <NavLink to=''>SING UP</NavLink>
+      <NavLink to=''>LOGIN</NavLink>
+    </div>
+      </div>
+  
+
+      <div className='flex w-[97%] mx-auto py-4 px-56 shadow-sm justify-between'>
+        <NavLink to='/' className='text-indigo-300 font-semibold'>MovieFlix</NavLink>
+        <ul className="flex text-gray-800 gap-4 uppercase font-semibold">
+          <li className='underline-animation'><NavLink to="/">Home</NavLink></li>
+          <li className='underline-animation'><NavLink to="/about">About</NavLink></li>
+          <li className='underline-animation'><NavLink to="/movie">Movie</NavLink></li>
+          <li className='underline-animation'><NavLink to="/contact">Contact</NavLink></li>
+        </ul>
+      </div>
+
+    </div>
   )
 }
