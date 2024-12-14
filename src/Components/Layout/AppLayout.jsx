f7ad966676bbddcb17b74e98474cee1984dc4a85
Pritom Chowdhury <coderpritom03@gmail.com>
2024-12-14 08:41:26 -0800

    Loading UI add it

--- src/Components/Layout/AppLayout.jsx
+++ src/Components/Layout/AppLayout.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
 import Footer from './Footer'
+import { useNavigation } from 'react-router-dom'
+import Loader from '../Ui/Loading'
 
 export default function AppLayout() {
+
+  const Navigation = useNavigation()
+  if(Navigation.state == "loading") return <Loader />
+  
   return (
     <>
     <Header/>
---
+++ src/Components/Ui/Loading.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactLoading from 'react-loading';
+
+const Loader = () => (
+    <ReactLoading type="spin" color="#431db5" height={200} width={200} className='mx-auto mt-72' />
+);
+
+export default Loader;
